279b3698dd22206b90ca22b6215adf88c7ad38df
Hairul Lana <hairullana99@gmail.com>
2022-02-26 16:54:09 +0800

    parameter form

--- darbukaToneClassification/views.py
+++ darbukaToneClassification/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
 
 def index(request):
-  return render(request, 'index.html')
+  data = {
+    'windowLength': 0.02,
+    'frameLength': 0.01,
+    'mfccCoefficients': 13,
+    'k': 1
+  }
+  
+  if request.method == 'POST':
+    data['windowLength'] = request.POST['windowLength']
+    data['frameLength'] = request.POST['frameLength']
+    data['mfccCoefficients'] = request.POST['mfccCoefficients']
+    data['k'] = request.POST['k']
+
+  return render(request, 'index.html', data)
