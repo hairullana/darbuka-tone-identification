047ab2a7b793d1d694962d8fa239f0bfbc5136e6
Hairul Lana <hairullana99@gmail.com>
2022-02-26 14:45:55 +0800

    add templates

--- darbukaToneClassification/settings.py
+++ darbukaToneClassification/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'darbukaToneClassification.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +116,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
