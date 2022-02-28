180dc7cea126116b6560f826328dfa09a38fc26c
Hairul Lana <hairullana99@gmail.com>
2022-02-28 14:50:16 +0800

    apply identification to identification.html

--- darbukaToneClassification/views.py
+++ darbukaToneClassification/views.py
@@ -4,24 +4,7 @@ from django.shortcuts import render
 from DataLatih.functions.automaticClassificationFunc import klasifikasiNada, klasifikasiNadaDasar
 
 def index(request):
-  data = {
-    'windowLength': 0.02,
-    'frameLength': 0.01,
-    'mfccCoefficients': 13,
-    'k': 1
-  }
-
-  if request.method == 'POST':
-    if 'automaticClassification' in request.POST :
-      data['hasilKlasifikasiDum'], data['hasilKlasifikasiTak'], data['hasilKlasifikasiSlap'], data['hasilPresentaseKlasifikasi'] = klasifikasiNada(data['windowLength'], data['frameLength'], data['mfccCoefficients'], data['k'])
-      data['text'] = klasifikasiNadaDasar(data['windowLength'], data['frameLength'], data['mfccCoefficients'], data['k'])
-    else :
-      data['windowLength'] = request.POST['windowLength']
-      data['frameLength'] = request.POST['frameLength']
-      data['mfccCoefficients'] = request.POST['mfccCoefficients']
-      data['k'] = request.POST['k']
-
-  return render(request, 'index.html', data)
+  return render(request, 'index.html')
 
 def training(request):
   return render(request, 'training.html')
@@ -30,14 +13,38 @@ def basicTone(request):
   context = {
     'title': 'Basic Tone Identification',
     'description': 'Identify the basic hadrah tone (DUM, TAK/KA, SLAP) with Mel Frequency Cepstral Coefficient (MFCC) and K-Nearest Neighbor (KNN) algorithm',
+    'windowLength': 0.02,
+    'frameLength': 0.01,
+    'k': 1,
+    'automaticIdentification': 'basicTone',
   }
+
+  if request.method == 'POST':
+    if 'basicTone' in request.POST :
+      context['hasilKlasifikasiDum'], context['hasilKlasifikasiTak'], context['hasilKlasifikasiSlap'], context['hasilPresentaseKlasifikasi'] = klasifikasiNada(context['windowLength'], context['frameLength'], 13, context['k'])
+    else :
+      context['windowLength'] = request.POST['windowLength']
+      context['frameLength'] = request.POST['frameLength']
+      context['k'] = request.POST['k']
 
   return render(request, 'identification.html', context)
 
 def tonePattern(request):
   context = {
     'title': 'Tone Pattern Identification',
-    'description': 'Identify hadrah tone pattern (DUM, TAK/KA, SLAP) with Mel Frequency Cepstral Coefficient (MFCC), Onset Detection and K-Nearest Neighbor (KNN) algorithm'
+    'description': 'Identify hadrah tone pattern (DUM, TAK/KA, SLAP) with Mel Frequency Cepstral Coefficient (MFCC), Onset Detection and K-Nearest Neighbor (KNN) algorithm',
+    'windowLength': 0.02,
+    'frameLength': 0.01,
+    'k': 1,
+    'automaticIdentification': 'tonePattern',
   }
+
+  if request.method == 'POST':
+    if 'tonePattern' in request.POST :
+      context['text'] = klasifikasiNadaDasar(context['windowLength'], context['frameLength'], 13, context['k'])
+    else :
+      context['windowLength'] = request.POST['windowLength']
+      context['frameLength'] = request.POST['frameLength']
+      context['k'] = request.POST['k']
 
   return render(request, 'identification.html', context)
