1e1c6c9aefae277850c91881f4e8ec0f9bb150f3
Hairul Lana <hairullana99@gmail.com>
2022-02-28 15:38:51 +0800

    fix ui result of basic tone

--- DataLatih/functions/automaticClassificationFunc.py
+++ DataLatih/functions/automaticClassificationFunc.py
@@ -4,16 +4,15 @@ import librosa
 
 def klasifikasiNada(windowLength, frameLength, mfccCoefficients, k):
 
-  hasilKlasifikasiDum = ""
-  hasilKlasifikasiTak = ""
-  hasilKlasifikasiSlap = ""
+  hasilKlasifikasiDum = []
+  hasilKlasifikasiTak = []
+  hasilKlasifikasiSlap = []
   jumlahKlasifikasiDumBenar = 0
   jumlahKlasifikasiTakBenar = 0
   jumlahKlasifikasiSlapBenar = 0
   hasilPresentaseKlasifikasi = ""
 
   # TESTING DUM
-  hasilKlasifikasiDum += '<br/>Sedang melakukan klasifikasi nada DUM (K=' + str(k) + ')<br/>'
   for i in range(20) :
     indeks = i + 51
     filename = 'static/dataset/toneBasic/dum/dum' + str(indeks) + '.wav'
@@ -22,10 +21,9 @@ def klasifikasiNada(windowLength, frameLength, mfccCoefficients, k):
     file2 = file[len(file)-1]
     if hasil == 'DUM':
       jumlahKlasifikasiDumBenar += 1
-    hasilKlasifikasiDum += '<br/>Hasil dari klasifikasi ' + file2 + ' adalah nada ' + hasil
+    hasilKlasifikasiDum.append(hasil)
 
   # TESTING TAK
-  hasilKlasifikasiTak += '<br/>Sedang melakukan klasifikasi nada TAK (K=' + str(k) + ')<br/>'
   for i in range(20) :
     indeks = i + 51
     filename = 'static/dataset/toneBasic/tak/tak' + str(indeks) + '.wav'
@@ -34,10 +32,9 @@ def klasifikasiNada(windowLength, frameLength, mfccCoefficients, k):
     file2 = file[len(file)-1]
     if hasil == 'TAK':
       jumlahKlasifikasiTakBenar += 1
-    hasilKlasifikasiTak += '<br/>Hasil dari klasifikasi ' + file2 + ' adalah nada ' + hasil
+    hasilKlasifikasiTak.append(hasil)
 
   # TESTING SLAP
-  hasilKlasifikasiSlap += '<br/>Sedang melakukan klasifikasi nada SLAP (K=' + str(k) + ')<br/>'
   for i in range(20) :
     indeks = i + 51
     filename = 'static/dataset/toneBasic/slap/slap' + str(indeks) + '.wav'
@@ -46,14 +43,14 @@ def klasifikasiNada(windowLength, frameLength, mfccCoefficients, k):
     file2 = file[len(file)-1]
     if hasil == 'SLAP':
       jumlahKlasifikasiSlapBenar += 1
-    hasilKlasifikasiSlap += '<br/>Hasil dari klasifikasi ' + file2 + ' adalah nada' + hasil
+    hasilKlasifikasiSlap.append(hasil)
   
   jumlahKlasifikasiBenar = jumlahKlasifikasiDumBenar + jumlahKlasifikasiTakBenar + jumlahKlasifikasiSlapBenar
 
-  hasilPresentaseKlasifikasi += "Akurasi Nada DUM = " + str(jumlahKlasifikasiDumBenar) + "/20 (" + str(jumlahKlasifikasiDumBenar/20*100) + "%)<br/>"
-  hasilPresentaseKlasifikasi += "Akurasi Nada TAK = " + str(jumlahKlasifikasiTakBenar) + "/20 (" + str(jumlahKlasifikasiTakBenar/20*100) + "%)<br/>"
-  hasilPresentaseKlasifikasi += "Akurasi Nada SLAP = " + str(jumlahKlasifikasiSlapBenar) + "/20 (" + str(jumlahKlasifikasiSlapBenar/20*100) + "%)<br/>"
-  hasilPresentaseKlasifikasi += "Akurasi Sistem = " + str(jumlahKlasifikasiBenar) + "/60 (" + str(jumlahKlasifikasiBenar/60*100) + "%)"
+  hasilPresentaseKlasifikasi += "Total = " + str(jumlahKlasifikasiBenar) + "/60 (" + str(jumlahKlasifikasiBenar/60*100) + "%)"
+  hasilPresentaseKlasifikasi += "DUM Tone = " + str(jumlahKlasifikasiDumBenar) + "/20 (" + str(jumlahKlasifikasiDumBenar/20*100) + "%)<br/>"
+  hasilPresentaseKlasifikasi += "TAK Tone = " + str(jumlahKlasifikasiTakBenar) + "/20 (" + str(jumlahKlasifikasiTakBenar/20*100) + "%)<br/>"
+  hasilPresentaseKlasifikasi += "SLAP Tone = " + str(jumlahKlasifikasiSlapBenar) + "/20 (" + str(jumlahKlasifikasiSlapBenar/20*100) + "%)<br/>"
 
   return hasilKlasifikasiDum, hasilKlasifikasiTak, hasilKlasifikasiSlap, hasilPresentaseKlasifikasi
 
