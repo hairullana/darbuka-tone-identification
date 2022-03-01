85af329affebf9fcc959ec699bf8f6e9c21636d0
Hairul Lana <hairullana99@gmail.com>
2022-03-01 11:45:33 +0800

    delete tkinter func

--- DataLatih/functions/resetResultFunc.py
+++
@@ -1,6 +0,0 @@
-def resetHasil(klasifikasiDum, klasifikasiTak, klasifikasiSlap, presentaseKlasifikasi, info):
-  klasifikasiDum.config(text="")
-  klasifikasiTak.config(text="")
-  klasifikasiSlap.config(text="")
-  presentaseKlasifikasi.config(text="")
-  info.config(text="")
