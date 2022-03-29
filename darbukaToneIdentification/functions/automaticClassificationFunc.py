f4cfbc06eba5540bf8d0c3a91f63cf29187841b4
Hairul Lana <hairullana99@gmail.com>
2022-03-29 11:14:53 +0800

    update filename path

--- darbukaToneIdentification/functions/automaticClassificationFunc.py
+++ darbukaToneIdentification/functions/automaticClassificationFunc.py
@@ -14,7 +14,7 @@ def basicToneAutomaticIdentification(frameLength, hopLength, mfccCoefficient, k)
   # TESTING DUM
   for i in range(totalTesting) :
     indeks = i + 51
-    filename = 'static/dataset/toneBasicNoise/dum/dum' + str(indeks) + '.wav'
+    filename = 'D:/Ngoding/darbukaToneClassification/static/dataset/toneBasicNoise/dum/dum' + str(indeks) + '.wav'
     result = basicToneIdentification(filename, k, frameLength, hopLength, mfccCoefficient)
     if result == 'DUM':
       totalTrueDum += 1
@@ -23,7 +23,7 @@ def basicToneAutomaticIdentification(frameLength, hopLength, mfccCoefficient, k)
   # TESTING TAK
   for i in range(totalTesting) :
     indeks = i + 51
-    filename = 'static/dataset/toneBasicNoise/tak/tak' + str(indeks) + '.wav'
+    filename = 'D:/Ngoding/darbukaToneClassification/static/dataset/toneBasicNoise/tak/tak' + str(indeks) + '.wav'
     result = basicToneIdentification(filename, k, frameLength, hopLength, mfccCoefficient)
     if result == 'TAK':
       totalTrueTak += 1
@@ -32,7 +32,7 @@ def basicToneAutomaticIdentification(frameLength, hopLength, mfccCoefficient, k)
   # TESTING SLAP
   for i in range(totalTesting) :
     indeks = i + 51
-    filename = 'static/dataset/toneBasicNoise/slap/slap' + str(indeks) + '.wav'
+    filename = 'D:/Ngoding/darbukaToneClassification/static/dataset/toneBasicNoise/slap/slap' + str(indeks) + '.wav'
     result = basicToneIdentification(filename, k, frameLength, hopLength, mfccCoefficient)
     if result == 'SLAP':
       totalTrueSlap += 1
@@ -83,7 +83,7 @@ def tonePatternAutomaticIdentification(frameLength, hopLength, mfccCoefficient, 
 
   for tonePattern in tonePatterns:
     for i in range(10):
-      filename = 'static/dataset/tonePattern/' + tonePattern + '2_' + str(i+1) + '.wav'
+      filename = 'D:/Ngoding/darbukaToneClassification/static/dataset/tonePattern/' + tonePattern + '2_' + str(i+1) + '.wav'
       
       identification = []
       result = tonePatternIdentification(filename, k, frameLength, hopLength, mfccCoefficient)
